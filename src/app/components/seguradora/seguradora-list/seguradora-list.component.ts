3a0a880c4ce1b1e8b6cae251e9c636a425c7ee41
lucasbrum <brum.lucas@icloud.com>
2023-02-19 13:42:47 -0300

    Ajustando titulo do header

---
+++ src/app/components/seguradora/seguradora-list/seguradora-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seguradora-list',
+  templateUrl: './seguradora-list.component.html',
+  styleUrls: ['./seguradora-list.component.css']
+})
+export class SeguradoraListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
