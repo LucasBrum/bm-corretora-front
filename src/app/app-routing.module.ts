ba41dfba5f3e21f0451d4bda440e65fd3dc71d7c
lucasbrum <brum.lucas@icloud.com>
2023-02-20 10:28:02 -0300

    Adicionando rota para Seguradoras

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { SeguradoraListComponent } from './components/seguradora/seguradora-list/seguradora-list.component';
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,7 +7,8 @@ import { NavComponent } from './components/nav/nav.component';
 const routes: Routes = [
   {
     path: '', component: NavComponent, children: [
-      { path: 'home', component: HomeComponent }
+      { path: 'home', component: HomeComponent },
+      {path: 'seguradoras', component: SeguradoraListComponent}
     ]
   }
 ];
