86f92a5b6d4f434d87cd411c3fdcaeae6905fb44
Lucas Brum <brum.lucas@icloud.com>
2023-02-20 16:01:37 -0300

    Criando rota para o Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './components/login/login.component';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { SeguradoraListComponent } from './components/seguradora/seguradora-list/seguradora-list.component';
 import { HomeComponent } from './components/home/home.component';
@@ -6,6 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   {
     path: '', component: NavComponent, children: [
       { path: 'home', component: HomeComponent },
